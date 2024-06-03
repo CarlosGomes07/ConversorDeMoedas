102e030b81f8f07b142479ad67d65f8f0b2dde2d
CarlosGomes07 <carlosgomes1999@hotmail.com>
2024-06-03 17:50:35 -0300

    feat

---
+++ conversorApp/src/main/java/com/currencyconverter/DigitarMoeda.java
@@ -0,0 +1,25 @@
+package com.currencyconverter;
+
+import java.util.Scanner;
+
+public class DigitarMoeda {
+    public static void digitar(Scanner scanner){
+        try {
+
+            System.out.print("Digite a abreviação da moeda!\n");
+            System.out.print("Exemplos USD, BRL, EUR, JPY e outros...\n");
+            System.out.print("Digite a moeda de origem: ");
+            String moedaOrigem = scanner.next().toUpperCase();
+            System.out.print("Digite a moeda de destino: ");
+            String moedaDestino = scanner.next().toUpperCase();
+            Conversor.converterMoeda(moedaOrigem, moedaDestino, scanner);
+        } catch (Exception e) {
+            
+            System.out.println("*********************** - ERRO - *******************************");
+            System.out.println("Ocorreu um erro: " + e.getMessage());
+            System.out.println("****************************************************************");
+            // Finalizar a aplicação
+            System.exit(1);
+        }
+    }
+}
---
+++ conversorApp/src/main/java/com/currencyconverter/Main.java
@@ -0,0 +1,8 @@
+package com.currencyconverter;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu exibirMenu = new Menu();
+        exibirMenu.exibir();
+    }
+}
